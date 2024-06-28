bda94d841d69569ec8764306226bd4f5b01706a5
fionaharia <hariafiona@gmail.com>
2024-06-29 00:05:36 +0530

    dark mode

--- components/NavbarComp.tsx
+++ components/NavbarComp.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import {
   Navbar,
   NavbarBrand,
@@ -20,6 +20,10 @@ const NavbarComp = () => {
   const toggleDarkMode = () => {
     setDarkMode(!darkMode);
   };
+
+  useEffect(() => {
+    setDarkMode(darkMode);
+  },[])
 
   const menuItems = [
     { id: 1, label: "home", href: "#home" },
