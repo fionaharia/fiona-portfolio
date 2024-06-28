33031a189f4823fb4ca5a0f8cafed7b72c1ec721
fionaharia <hariafiona@gmail.com>
2024-06-29 00:09:27 +0530

    dark

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <AOSInit />
       <body>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
